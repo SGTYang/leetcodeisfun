be68edb633e430881434128d047b2c64b0e67b8b
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2023-01-27 14:00:02 +0900

    Time: 397 ms (75.23%), Space: 32.5 MB (16.69%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -3,8 +3,11 @@ class Solution:
         
         word_set = set(words)
         res = []
+        dp = {}
         
         def dfs(word):
+            if word in dp:
+                return dp[word]
             
             for i in range(1,len(word)):
                 prefix = word[:i]
@@ -12,8 +15,12 @@ class Solution:
                 
                 if ((prefix in word_set and suffix in word_set) or 
                     (prefix in word_set and dfs(suffix))):
-                    return True
-                   
+                    dp[word] = True
+                    return dp[word]
+            
+            dp[word] = False
+            return dp[word]
+            
         for w in words:
             if dfs(w):
                 res.append(w)
