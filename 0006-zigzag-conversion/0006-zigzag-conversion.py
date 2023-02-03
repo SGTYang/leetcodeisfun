5858b0c5884ce1cc712cff740fa59eabd8226b31
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2023-02-04 01:08:50 +0900

    Time: 59 ms (76.82%), Space: 14 MB (72.54%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        res = [""]*numRows
+
+        pointer = 0
+        
+        l = len(s)
+        
+        for i in range(l):
+            if pointer == 0:
+                operator = 1
+            elif pointer == numRows-1:
+                operator = -1
+            
+            res[pointer] += s[i]
+            
+            pointer += operator
+            
+        return "".join(res) 
