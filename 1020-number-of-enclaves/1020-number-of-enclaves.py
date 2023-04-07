d97b8a6276638ed25f56bf86cd726d82dccbf440
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-04-08 06:06:21 +0900

    Time: 602 ms (95.03%), Space: 75.6 MB (62.38%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numEnclaves(self, A: List[List[int]]) -> int:
+        row, col = len(A), len(A[0])
+
+        if not A or not A[0]:
+            return 0
+
+        def dfs(x,y,A):
+            if 0<=x<row and 0<=y<col and A[x][y] ==1:
+                A[x][y] = "T"
+                dfs(x+1,y,A)
+                dfs(x-1,y,A)
+                dfs(x,y+1,A)
+                dfs(x,y-1,A)
+
+
+        for x in range(row):
+            dfs(x,0,A)
+            dfs(x,col-1,A)
+
+        for y in range(1,col-1):
+            dfs(0,y,A)
+            dfs(row-1,y,A)
+
+        count=0
+        for x in range(row):
+            for y in range(col):
+                if A[x][y]==1:
+                    count+=1
+
+        return count
