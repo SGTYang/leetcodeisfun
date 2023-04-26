0d34ac9c51d3f2aabba4ca2194ee02d664cdd35c
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-04-27 02:50:05 +0900

    Time: 369 ms (42.08%), Space: 37.7 MB (14.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -14,3 +14,18 @@ class Solution:
             return dp[(idx, total)]
         
         return backtrack(0,0)
+    
+        res = [0]
+        
+        def backtrack(idx, total):
+            if idx == len(nums) and total == target:
+                res[0] += 1
+                return
+            if idx >= len(nums):
+                return
+            
+            backtrack(idx+1, total+nums[idx])
+            backtrack(idx+1, total-nums[idx])
+
+        backtrack(0,0)
+        return res[0]
