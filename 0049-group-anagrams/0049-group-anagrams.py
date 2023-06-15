9798f671ecd35b6897771352f1605a2e1d483db5
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-06-15 20:52:05 +0900

    Time: 128 ms (30.84%), Space: 22.3 MB (8.19%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,19 +5,7 @@ class Solution:
         for s in strs:
             count = [0]*26
             for c in s:
-                count[ord(c) - ord("a")] += 1
-            
+                count[ord(c)-ord("a")] += 1
             res[tuple(count)].append(s)
         
         return res.values()
-        
-        
-        # slower
-        word_dict = defaultdict(list)
-        
-        for s in strs:
-            changed = "".join(sorted(s))
-            word_dict[changed].append(s)
-        
-        return word_dict.values()
-            
