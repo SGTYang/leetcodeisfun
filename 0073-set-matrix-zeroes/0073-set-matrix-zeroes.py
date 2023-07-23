b3749a5e8516dfa20ce287be234f01e2a33a1780
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-23 09:34:52 +0900

    Time: 148 ms (24.81%), Space: 17.4 MB (21.03%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,28 +3,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
-        ROWS, COLS = len(matrix), len(matrix[0])
+        m, n = len(matrix), len(matrix[0])
         row_zero = False
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    if r > 0:
-                        matrix[r][0] = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
                     else:
                         row_zero = True
         
-        for r in range(1,ROWS):
-            for c in range(1,COLS):
-                if matrix[r][0] == 0 or matrix[0][c] == 0:
-                    matrix[r][c] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
         
         if matrix[0][0] == 0:
-            for r in range(ROWS):
-                matrix[r][0] = 0
+            for i in range(m):
+                matrix[i][0] = 0
         
         if row_zero:
-            for c in range(COLS):
-                matrix[0][c] = 0
+            for j in range(n):
+                matrix[0][j] = 0
