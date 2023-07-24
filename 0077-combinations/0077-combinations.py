2bbc2afdaee3594c848ddfed565bc25484fb2c93
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-24 14:40:03 +0900

    Time: 764 ms (5.01%), Space: 18.3 MB (34.46%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,17 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-        subset = []
         
-        def dfs(idx):
-            if len(subset) == k:
-                res.append(subset.copy())
+        def dfs(i, bucket):
+            if i > n+1:
+                return
+            if len(bucket) == k:
+                res.append(bucket.copy())
                 return
             
-            for i in range(idx, n+1):
-                subset.append(i)
-                dfs(i+1)
-                subset.pop()
+            bucket += [i]
+            dfs(i + 1, bucket)
+            bucket.pop()
+            dfs(i + 1, bucket)
+            
+        dfs(1, [])
         
-        dfs(1)
         return res
