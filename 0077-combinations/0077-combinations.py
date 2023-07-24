03ca08daa5efd53c0fcacd66c36caf6c7f15cfbd
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-24 15:01:30 +0900

    Time: 780 ms (5.01%), Space: 18.4 MB (34.46%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,5 +1,7 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # Time: O(k * 2^n)
+        # Space: O(k)
         res = []
         
         def dfs(i, bucket):
@@ -16,4 +18,20 @@ class Solution:
             
         dfs(1, [])
         
+        return res
+    
+        res = []
+        subset = []
+        
+        def dfs(idx):
+            if len(subset) == k:
+                res.append(subset.copy())
+                return
+            
+            for i in range(idx, n+1):
+                subset.append(i)
+                dfs(i+1)
+                subset.pop()
+        
+        dfs(1)
         return res
