ea594f2597e9e0394e120cbb356d0e1457d5f4a2
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-26 16:32:26 +0900

    Time: 52 ms (37.13%), Space: 16.5 MB (33.39%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -8,7 +8,7 @@ class Solution:
                 dp[i] = 0
             else:
                 dp[i] = dp[i+1]
-            
+                
             if i+1 < len(s) and (s[i]=="1" or (s[i]=="2" and s[i+1] in "0123456")):
                 dp[i] += dp[i+2]
         
