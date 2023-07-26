56a7a4475816e47255914556b474a06a970f6d2a
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-26 17:35:54 +0900

    Time: 1042 ms (84.30%), Space: 16.9 MB (13.62%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,12 +1,23 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        if not nums: return 0
-        n = len(nums)
-        m, dp, cnt = 0, [1] * n, [1] * n
-        for i in range(n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    if dp[i] < dp[j]+1: dp[i], cnt[i] = dp[j]+1, cnt[j]
-                    elif dp[i] == dp[j]+1: cnt[i] += cnt[j]
-            m = max(m, dp[i])                        
-        return sum(c for l, c in zip(dp, cnt) if l == m)
+        dp = {}
+        len_lis, res = 0, 0
+        
+        for i in range(len(nums)-1, -1, -1):
+            max_len, max_cnt = 1, 1
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    length, count = dp[j]
+                    if length + 1 > max_len:
+                        max_len, max_cnt = length + 1, count
+                    elif length + 1 == max_len:
+                        max_cnt += count
+                        
+            if max_len > len_lis:
+                len_lis, res = max_len, max_cnt
+            elif max_len == len_lis:
+                res += max_cnt
+            
+            dp[i] = [max_len, max_cnt]
+        
+        return res
