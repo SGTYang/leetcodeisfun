eec6dcfa0b3256522d5753115c28e123728a72fb
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-27 17:27:31 +0900

    Time: 279 ms (78.13%), Space: 18 MB (55.64%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,31 +1,30 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac = set()
-        atl = set()
-        
-        def dfs(i,j, visited, preHeight):
-            if (i < 0 or j < 0 or i >= ROWS or j >= COLS or 
-                (i,j) in visited or heights[i][j] < preHeight):
-                return
-            visited.add((i,j))
-            dfs(i+1,j,visited,heights[i][j])
-            dfs(i-1,j,visited,heights[i][j])
-            dfs(i,j+1,visited,heights[i][j])
-            dfs(i,j-1,visited,heights[i][j])
-        
-        for c in range(COLS):
-            dfs(0, c, atl, heights[0][c])
-            dfs(ROWS-1, c, pac, heights[ROWS-1][c])
-        
-        for r in range(ROWS):
-            dfs(r, 0, atl, heights[r][0])
-            dfs(r, COLS-1, pac, heights[r][COLS-1])
-        
+        m, n = len(heights), len(heights[0])
+        pac, atl = set(), set()
         res = []
-        for i in range(ROWS):
-            for j in range(COLS):
-                if (i,j) in pac and (i,j) in atl:
-                    res.append([i,j])
+        
+        def dfs(i, j, visited, prev):
+            if i >= m or j >= n or i < 0 or j < 0 or (i,j) in visited or heights[i][j] < prev:
+                return
+        
+            visited.add((i, j))
+            dfs(i - 1, j, visited, heights[i][j])
+            dfs(i + 1, j, visited, heights[i][j])
+            dfs(i, j - 1, visited, heights[i][j])
+            dfs(i, j + 1, visited, heights[i][j])
+        
+        for i in range(n):
+            dfs(0, i, pac, heights[0][i])
+            dfs(m - 1, i, atl, heights[m - 1][i])
+        
+        for i in range(m):
+            dfs(i, 0, pac, heights[i][0])
+            dfs(i, n - 1, atl, heights[i][n - 1])
+        
+        for i in range(m):
+            for j in range(n):
+                if (i, j) in pac and (i, j) in atl:
+                    res.append((i, j))
         
         return res
