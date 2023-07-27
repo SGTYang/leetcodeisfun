0e0c32bd054736804ad838ff94bf05b487ec70d5
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-27 17:34:48 +0900

    Time: 327 ms (72.79%), Space: 18.9 MB (66.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])
         res = 0
         
-        def dfs(i,j):
-            if (i < 0 or j < 0 or 
-                i == ROWS or j == COLS or
-                grid[i][j] == "0"):
-                return 
-            grid[i][j] = "0"
+        def dfs(i, j, visited):
+            if i >= m or j >= n or i < 0 or j < 0 or (i, j) in visited or grid[i][j] == "0":
+                return
             
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1":
+            visited.add((i, j))
+            grid[i][j] = "0"
+            dfs(i - 1, j, visited)
+            dfs(i + 1, j, visited)
+            dfs(i, j - 1, visited)
+            dfs(i, j + 1, visited)
+            
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    dfs(i, j, set())
                     res += 1
-                    dfs(r,c)
         
         return res
