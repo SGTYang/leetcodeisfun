7ed714fa617376736b5e5fac9466a3c09357e410
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-28 15:45:10 +0900

    Time: 262 ms (10.16%), Space: 21 MB (91.37%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -18,26 +18,3 @@ class Solution:
                 l = mid + 1 
             
         return l
-    
-        n = len(matrix)
-        left, right = matrix[0][0], matrix[n-1][n-1]
-        
-        while left < right:
-            mid = (left + right) // 2
-            
-            count = 0
-            row, col = n - 1, 0
-            
-            while row >= 0 and col < n:
-                if matrix[row][col] > mid:
-                    row -= 1
-                else:
-                    count += row + 1
-                    col += 1
-
-            if count < k:
-                left = mid + 1
-            else:
-                right = mid
-        
-        return left
