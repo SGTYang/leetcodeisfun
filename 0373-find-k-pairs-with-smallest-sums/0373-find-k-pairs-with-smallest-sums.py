f8e135fdb62a645a442c413f65cb2ce22bdac7f5
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-28 16:26:42 +0900

    Time: 984 ms (100.00%), Space: 43.6 MB (5.80%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,17 +1,15 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        minHeap = []
-        heapq.heapify(minHeap)
+        heap = []
         res = []
-        j = 0
-        for i in range(min(len(nums1), k)):
-            heapq.heappush(minHeap, (nums1[i]+nums2[0], i, j, j))
         
+        for i in range(len(nums1)):
+            heapq.heappush(heap, (nums1[i]+nums2[0], i, 0))
         
-        while minHeap and len(res) < k:
-            minSum, n, m, j = heapq.heappop(minHeap)
-            res.append([nums1[n],nums2[m]])
+        while len(res) < k and heap:
+            total, i, j = heapq.heappop(heap)
+            res.append([nums1[i], nums2[j]])
             if j < len(nums2)-1:
-                heapq.heappush(minHeap, (nums1[n]+nums2[j+1], n, j+1, j+1))
+                heapq.heappush(heap, (nums1[i]+nums2[j+1], i, j+1))
         
         return res
