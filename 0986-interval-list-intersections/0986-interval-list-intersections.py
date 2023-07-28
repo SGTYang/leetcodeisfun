45758bf52f4e45d8a4a538ba54f1de927512c05e
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-28 17:32:05 +0900

    Time: 133 ms (100.00%), Space: 17.3 MB (74.90%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -1,24 +1,24 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        firstList.sort(key = lambda x : x[0])
-        secondList.sort(key = lambda x: x[1])
-        
+        firstList.sort()
+        secondList.sort()
+        first_idx = 0
+        second_idx = 0
         res = []
         
-        first, second = 0, 0
-        
-        while first < len(firstList) and second < len(secondList):
-            first_start, first_end = firstList[first]
-            second_start, second_end = secondList[second]
+        while first_idx < len(firstList) and second_idx < len(secondList):
+            first_start, first_end = firstList[first_idx]
+            second_start, second_end = secondList[second_idx]
             
-            overlap_start, overlap_end = max(first_start, second_start), min(first_end, second_end)
+            overlap_start = max(first_start, second_start)
+            overlap_end = min(first_end, second_end)
             
             if overlap_start <= overlap_end:
                 res.append([overlap_start, overlap_end])
             
             if first_end < second_end:
-                first +=1
+                first_idx += 1
             else:
-                second +=1
+                second_idx += 1
         
         return res
