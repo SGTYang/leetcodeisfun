7339e2ef040506449ea1450cf4e98b775cfbc1e7
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-07-29 17:09:42 +0900

    Time: 53 ms (99.46%), Space: 17 MB (13.13%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,5 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return True
+            
+            if nums[mid] == nums[left]:
+                left += 1
+                continue
+            if nums[mid] == nums[right]:
+                right -= 1
+                continue
+            
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or nums[left] > target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        
+        return False
+    
         l, r = 0, len(nums)-1
         
         while l <= r:
@@ -13,6 +40,7 @@ class Solution:
             if nums[mid] == nums[r]:
                 r -= 1
                 continue
+                
             # Left sorted portion
             if nums[l] <= nums[mid]:
                 if nums[mid] < target or nums[l] > target:
