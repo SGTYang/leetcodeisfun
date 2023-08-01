453ad650dc9da3c5dbbd2c2c18f590b0590823eb
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-01 13:57:15 +0900

    Time: 303 ms (78.18%), Space: 18.2 MB (67.82%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,39 +1,54 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # Time: O(k * n^k)
-        # Space: O(k)
-        res = []
-        subset = []
-        
-        def dfs(idx):
-            if len(subset) == k:
-                res.append(subset.copy())
-                return
-            
-            for i in range(idx, n+1):
-                subset.append(i)
-                dfs(i+1)
-                subset.pop()
-        
-        dfs(1)
-        return res
-    
-        # Time: O(k * 2^n)
-        # Space: O(k)
         res = []
         
-        def dfs(i, bucket):
-            if i > n+1:
-                return
-            if len(bucket) == k:
-                res.append(bucket.copy())
+        def dfs(idx, curr):
+            if len(curr) == k:
+                res.append(curr.copy())
                 return
             
-            bucket += [i]
-            dfs(i + 1, bucket)
-            bucket.pop()
-            dfs(i + 1, bucket)
-            
+            for i in range(idx, n + 1):
+                curr += [i]
+                dfs(i + 1, curr)
+                curr.pop()
+                
         dfs(1, [])
-        
         return res
+            
+#         # Time: O(k * n^k)
+#         # Space: O(k)
+#         res = []
+#         subset = []
+        
+#         def dfs(idx):
+#             if len(subset) == k:
+#                 res.append(subset.copy())
+#                 return
+            
+#             for i in range(idx, n+1):
+#                 subset.append(i)
+#                 dfs(i+1)
+#                 subset.pop()
+        
+#         dfs(1)
+#         return res
+    
+#         # Time: O(k * 2^n)
+#         # Space: O(k)
+#         res = []
+        
+#         def dfs(i, bucket):
+#             if i > n+1:
+#                 return
+#             if len(bucket) == k:
+#                 res.append(bucket.copy())
+#                 return
+            
+#             bucket += [i]
+#             dfs(i + 1, bucket)
+#             bucket.pop()
+#             dfs(i + 1, bucket)
+            
+#         dfs(1, [])
+        
+#         return res
