2ea44e537016d931edd09668459cf14c682cac12
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-01 12:49:54 +0900

    Time: 54 ms (99.71%), Space: 16.6 MB (5.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,40 +2,22 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
+        s1_cnt = defaultdict(int)
+        s2_cnt = defaultdict(int)
         
-        s1_cnt = [0] * 26
-        s2_cnt = [0] * 26
-        
-        # init array
         for i in range(len(s1)):
-            s1_cnt[ord(s1[i]) - ord("a")] += 1
-            s2_cnt[ord(s2[i]) - ord("a")] += 1
-        
-        matches = 0
-        for i in range(26):
-            if s1_cnt[i] == s2_cnt[i]:
-                matches += 1
+            s1_cnt[s1[i]] += 1
+            s2_cnt[s2[i]] += 1
         
         left = 0
+        
         for right in range(len(s1), len(s2)):
-            if matches == 26:
+            if s1_cnt == s2_cnt:
                 return True
-            
-            index = ord(s2[right]) - ord("a")
-            s2_cnt[index] += 1
-            if s1_cnt[index] == s2_cnt[index]:
-                matches += 1
-            elif s1_cnt[index] + 1 == s2_cnt[index]:
-                matches -= 1
-            
-            index = ord(s2[left]) - ord("a")
-            s2_cnt[index] -= 1
-            if s1_cnt[index] == s2_cnt[index]:
-                matches += 1
-            elif s1_cnt[index] - 1 == s2_cnt[index]:
-                matches -= 1
-            
+            s2_cnt[s2[right]] += 1
+            s2_cnt[s2[left]] -= 1
+            if s2_cnt[s2[left]] == 0:
+                s2_cnt.pop(s2[left])
             left += 1
-                
-        return matches == 26
-            
+        
+        return True if s1_cnt == s2_cnt else False
