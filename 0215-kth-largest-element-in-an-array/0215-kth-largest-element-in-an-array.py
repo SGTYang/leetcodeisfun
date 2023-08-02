8855d5016055d56db49ad509045811c24abba1c7
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-02 12:20:55 +0900

    Time: 1480 ms (14.81%), Space: 40 MB (5.61%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,22 +1,25 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Time: O(n)
         # Quick select
-        k = len(nums) - k # kth largest
         
-        def quick_select(l, r):
-            pivot, p_pointer = nums[r], l
-            for i in range(l, r):
+        k = len(nums) - k # Kth largest
+        
+        def quick_select(left, right):
+            pivot, pointer = nums[right], left
+            
+            for i in range(left, right):
                 if nums[i] <= pivot:
-                    nums[p_pointer], nums[i] = nums[i], nums[p_pointer]
-                    p_pointer += 1
+                    nums[i], nums[pointer] = nums[pointer], nums[i]
+                    pointer += 1
             
-            nums[p_pointer], nums[r] = pivot, nums[p_pointer]
+            nums[right], nums[pointer] = nums[pointer], nums[right]
             
-            if p_pointer > k:
-                return quick_select(l, p_pointer - 1)
-            elif p_pointer < k:
-                return quick_select(p_pointer + 1, r)
+            if k > pointer:
+                return quick_select(pointer + 1, right)
+            elif k < pointer:
+                return quick_select(left, pointer - 1)
             else:
-                return nums[p_pointer]
+                return nums[pointer]
         
         return quick_select(0, len(nums) - 1)
