d6e73bd4a19d02f5102980a6f330c2608fd31e54
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-02 10:36:01 +0900

    Time: 75 ms (95.33%), Space: 20.9 MB (58.38%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,24 +7,25 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while True:
-            if root.val < min(p.val,q.val):
-                root = root.right
-            elif root.val > max(p.val,q.val):
-                root = root.left
+        curr = root
+        
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
             else:
-                return root
+                return curr
         
-        if not root:
-            return root
+#         # Wrong answer for some how
+#         if not root:
+#             return root
+#         def dfs(node):
+#             if node.val < min(p.val, q.val):
+#                 dfs(node.right)
+#             elif node.val > max(p.val, q.val):
+#                 dfs(node.left)
+#             else:
+#                 return node
         
-        # Wrong answer for some how
-        def dfs(node):
-            if node.val < min(p.val, q.val):
-                dfs(node.right)
-            elif node.val > max(p.val, q.val):
-                dfs(node.left)
-            else:
-                return node
-        
-        return dfs(root)
+#         return dfs(root)
