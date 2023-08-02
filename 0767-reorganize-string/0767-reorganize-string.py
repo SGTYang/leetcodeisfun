f1b00dc2f96629a42360b8eee6e5e82f7caa288d
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-02 12:34:10 +0900

    Time: 45 ms (78.05%), Space: 16.4 MB (49.06%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,23 +1,25 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         s_cnt = Counter(s)
+        
         max_heap = [[-cnt, char] for char, cnt in s_cnt.items()]
-        res = ""
+        heapq.heapify(max_heap) # O(N)
         prev = None
-        heapq.heapify(max_heap)
+        res = ""
         
         while max_heap or prev:
             if prev and not max_heap:
                 return ""
             cnt, char = heapq.heappop(max_heap)
-            res += char
             cnt += 1
+            res += char
             
             if prev:
                 heapq.heappush(max_heap, prev)
-                prev = None
             
             if cnt != 0:
                 prev = [cnt, char]
+            else:
+                prev = None
         
         return res
