abc7392799167b50e6ffc21c9c2bcb43bd156d3b
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-02 15:45:10 +0900

    Time: 39 ms (94.81%), Space: 16.4 MB (38.20%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Bucket Sort
-        bucket = [0] * 3
+        # Partition with two pointers
+        i, left, right = 0, 0, len(nums) - 1
+        while i <= right:
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+                i -= 1
+            i += 1
         
-        for num in nums:
-            bucket[num] += 1
         
-        left = 0
-        for i in range(len(bucket)):
-            right = left + bucket[i]
-            for j in range(left, right):
-                nums[j] = i
-            left = right
+#         # Bucket Sort
+#         bucket = [0] * 3
+        
+#         for num in nums:
+#             bucket[num] += 1
+        
+#         left = 0
+#         for i in range(len(bucket)):
+#             right = left + bucket[i]
+#             for j in range(left, right):
+#                 nums[j] = i
+#             left = right
