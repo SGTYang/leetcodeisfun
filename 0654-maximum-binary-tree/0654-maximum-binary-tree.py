eb9e9c858571519492ac71f123ba96a6fd3aa449
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-02 13:00:37 +0900

    Time: 163 ms (98.14%), Space: 16.9 MB (90.87%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.py
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.py
@@ -6,29 +6,16 @@
 #         self.right = right
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        if not nums:
-            return
-        
-        element = max(nums)
-        index = nums.index(element)
-        
-        node = TreeNode(element)
-        
-        node.left = self.constructMaximumBinaryTree(nums[0:index])
-        node.right = self.constructMaximumBinaryTree(nums[index + 1:])
-        
-        return node
-        
-        def recursive(num_list):
-            nums_dict = {v:i for i, v in enumerate(num_list)}
-            if not num_list:
-                return None
-            root = max(num_list)
-            tree = TreeNode(root)
+        def build_tree(nums):
+            if not nums:
+                return
+            max_value = max(nums) # O(n)
+            max_index = nums.index(max_value) # O(n)
             
-            tree.left = recursive(num_list[:nums_dict[root]])
-            tree.right = recursive(num_list[nums_dict[root]+1:])
+            root = TreeNode(max_value)
+            root.left = build_tree(nums[0:max_index])
+            root.right = build_tree(nums[max_index + 1:len(nums)])
             
-            return tree
+            return root
         
-        return recursive(nums)
+        return build_tree(nums)
