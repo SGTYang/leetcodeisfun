a74de3d0d81bea09a10d6f14480f1fb3d8244230
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-04 12:23:03 +0900

    Time: 161 ms (36.08%), Space: 17.8 MB (20.51%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,32 +1,29 @@
 class TrieNode:
     def __init__(self, val):
-        self.child = {}
+        self.children = {}
         self.val = val
-        self.end_of_word = False
+        self.is_word = False
         
 class Solution:
     def longestWord(self, words: List[str]) -> str:
+        words.sort()
+        res = ""
+        max_len = 0
         trie = TrieNode(0)
         
-        max_len = 0
-        res = ""
-        
-        for word in sorted(words):
+        for word in words:
             t = trie
             cnt = 0
-            
             for c in word:
-                if c not in t.child:
-                    t.child[c] = TrieNode(cnt)
-                t = t.child[c]
-                if t.end_of_word:
+                if c not in t.children:
+                    t.children[c] = TrieNode(cnt)
+                t = t.children[c]
+                if t.is_word:
                     cnt += 1
-            
-            t.end_of_word = True
+            t.is_word = True
             t.val += 1
             
             if t.val == len(word) and t.val > max_len:
                 max_len = t.val
                 res = word
-        
         return res
