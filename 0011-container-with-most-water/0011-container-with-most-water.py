b61706e518f26bea91b776746f8e5c6e35c2eb01
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-05 11:53:49 +0900

    Time: 597 ms (91.91%), Space: 29.4 MB (18.64%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,11 +3,14 @@ class Solution:
         res = 0
         left, right = 0, len(height) - 1
         
-        while left <= right:
-            contains = min(height[left], height[right]) * (right - left)
-            res = max(res, contains)
+        while left < right:
+            min_height = min(height[left], height[right])
+            res = max(res, min_height * (right - left))
             if height[left] < height[right]:
                 left += 1
+            elif height[right] < height[left]:
+                right -= 1
             else:
+                left += 1
                 right -= 1
         return res
