8a1acd0ccc59bd3d790c033b0ea3a0f8a881bc29
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-06 14:00:39 +0900

    Time: 530 ms (93.96%), Space: 31.1 MB (66.46%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -2,18 +2,18 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # 2(p+c-x) = p+nc-x
         slow, fast = 0, 0
-        
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            
             if slow == fast:
                 break
         
         slow2 = 0
         
-        while slow != slow2:
+        while True:
             slow = nums[slow]
             slow2 = nums[slow2]
-        
-        return slow
+            if slow2 == slow:
+                break
+                
+        return slow2
