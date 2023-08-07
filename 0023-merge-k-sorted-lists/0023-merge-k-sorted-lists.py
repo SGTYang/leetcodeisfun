4139ac635f94fb812e765c6507fddd752e749fe1
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-07 10:19:35 +0900

    Time: 100 ms (87.90%), Space: 19.9 MB (91.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,34 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
+        if len(lists) == 0:
             return None
         
-        while len(lists) > 1:
-            mergedLists = []
+        def merge_list(l1, l2):
+            dummy = ListNode(0)
+            curr = dummy
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                    curr = curr.next
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    curr = curr.next
+                    l2 = l2.next
+            if l1:
+                curr.next = l1
+            if l2:
+                curr.next = l2
             
+            return dummy.next
+        
+        while len(lists) > 1:
+            merged_list = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i+1] if (i+1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists = mergedLists
+                l2 = lists[i+1] if i + 1 < len(lists) else None
+                
+                merged_list.append(merge_list(l1, l2))
+            
+            lists = merged_list
+        
         return lists[0]
-    
-    def mergeList(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
-        return dummy.next
