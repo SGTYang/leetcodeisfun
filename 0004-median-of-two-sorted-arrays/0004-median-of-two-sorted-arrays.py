9e4b43c58bac14052a9ae4796e6e7b2e7bfe5ab0
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-09 05:17:32 +0900

    Time: 82 ms (97.83%), Space: 16.6 MB (75.22%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,27 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        total = len(nums1) + len(nums2)
-        half = (total) // 2
-        
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-            
-        l, r = 0, len(nums1) - 1
+        l1, l2 = len(nums1), len(nums2)
+        total = l1 + l2
+        half = total // 2
+        
+        l, r = 0, l1 - 1
         while True:
-            i = (l + r) // 2
-            j = half - i - 2
+            mid = (l + r) // 2
+            rest = half - mid - 2
             
-            a_left = nums1[i] if i >= 0 else float('-inf')
-            a_right = nums1[i + 1] if i + 1 < len(nums1) else float('inf')
-            b_left = nums2[j] if j >= 0 else float('-inf')
-            b_right = nums2[j + 1] if j + 1 < len(nums2) else float('inf')
+            nums1_left = nums1[mid] if mid >= 0 else float('-inf')
+            nums1_right = nums1[mid + 1] if mid + 1 < len(nums1) else float('inf')
+            nums2_left = nums2[rest] if rest >= 0 else float('-inf')
+            nums2_right = nums2[rest + 1] if rest + 1 < len(nums2) else float('inf')
             
-            if a_left <= b_right and b_left < a_right:
+            if nums1_left <= nums2_right and nums2_left <= nums1_right:
                 if total % 2:
-                    return min(a_right, b_right)
-                else:
-                    return (max(a_left, b_left) + min(a_right, b_right)) / 2
-            elif a_left > b_right:
-                r = i - 1
+                    return min(nums1_right, nums2_right)
+                return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
+            elif nums1_left > nums2_right:
+                r = mid - 1
             else:
-                l = i + 1
+                l = mid + 1
+        
