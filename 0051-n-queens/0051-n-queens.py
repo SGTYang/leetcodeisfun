c809bbc4ea9a474ef0c790e518d48e8939081f3d
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-13 14:12:54 +0900

    Time: 53 ms (91.88%), Space: 16.7 MB (95.65%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,34 +1,28 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["." for i in range(n)] for j in range(n)]
         col = set()
-        negDiag = set()
-        posDiag = set()
-        
+        pos_diag = set()
+        neg_diag = set()
         res = []
-        board = [["."]*n for i in range(n)]
         
-        def backtrack(r):
+        def dfs(r):
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+                res.append(["".join(row) for row in board])
                 return
             
             for c in range(n):
-                if c in col or (r+c) in posDiag or (r-c) in negDiag:
-                    continue
-                
-                col.add(c)
-                posDiag.add(r+c)
-                negDiag.add(r-c)
-                board[r][c] = "Q"
-                
-                backtrack(r+1)
-                
-                col.remove(c)
-                posDiag.remove(r+c)
-                negDiag.remove(r-c)
-                board[r][c] = "."
+                if c not in col and r - c not in pos_diag and r + c not in neg_diag:
+                    board[r][c] = "Q"
+                    col.add(c)
+                    pos_diag.add(r - c)
+                    neg_diag.add(r + c)
+                    dfs(r + 1)
+                    col.remove(c)
+                    pos_diag.remove(r - c)
+                    neg_diag.remove(r + c)
+                    board[r][c] = "."
         
-        backtrack(0)
-        
+        dfs(0)
+            
         return res
