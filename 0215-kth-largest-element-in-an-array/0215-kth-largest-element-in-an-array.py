0434e6641fa33e2a7dc07d5b2f2d9f0d0fbd75b2
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-14 09:32:41 +0900

    Time: 1793 ms (10.70%), Space: 40 MB (5.45%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -2,33 +2,30 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         # Time: O(n)
         # Quick select
-        n = len(nums)
-        k = n - k
-        
-        def quick_select(l, pivot):
-            pointer = l
-            left = l
-            while left < pivot:
-                if nums[left] <= nums[pivot]:
-                    nums[left], nums[pointer] = nums[pointer], nums[left]
+        k = len(nums) - k
+        def quick_select(left, pivot):
+            l = pointer = left
+            while l < pivot:
+                if nums[pivot] >= nums[l]:
+                    nums[pointer], nums[l] = nums[l], nums[pointer]
                     pointer += 1
-                left += 1
+                l += 1
             nums[pivot], nums[pointer] = nums[pointer], nums[pivot]
-            
-            if pointer > k:
-                return quick_select(l, pointer - 1)
-            elif pointer < k:
+            if pointer < k:
                 return quick_select(pointer + 1, pivot)
+            elif pointer > k:
+                return quick_select(left, pointer - 1)
             else:
                 return nums[pointer]
-            
-        return quick_select(0, n - 1)
         
+        return quick_select(0, len(nums) - 1)
         
-        # # Time: O(klogn)
-        # max_heap = [-num for num in nums]
-        # heapq.heapify(max_heap)
-        # while k:
-        #     res = heapq.heappop(max_heap)
-        #     k -= 1
-        # return -res
+        # Time O(klogn)
+        # Max heap
+        max_heap = [-num for num in nums]
+        heapq.heapify(max_heap)
+        while k:
+            res = heapq.heappop(max_heap)
+            k -= 1
+        
+        return -res
