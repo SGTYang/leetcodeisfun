e1bb40f632c440d4221eb1d6dbca155abfc7d7e8
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-14 10:35:03 +0900

    Time: 238 ms (91.97%), Space: 18.1 MB (54.81%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,27 +4,26 @@ class Solution:
         pac, atl = set(), set()
         res = []
         
-        def dfs(i, j, visited, prev):
-            if i >= m or j >= n or i < 0 or j < 0 or (i,j) in visited or heights[i][j] < prev:
+        def dfs(i, j, prev, visited):
+            if i < 0 or j < 0 or i >= m or j >= n or (i, j) in visited or heights[i][j] < prev:
                 return
-        
             visited.add((i, j))
-            dfs(i - 1, j, visited, heights[i][j])
-            dfs(i + 1, j, visited, heights[i][j])
-            dfs(i, j - 1, visited, heights[i][j])
-            dfs(i, j + 1, visited, heights[i][j])
+            dfs(i + 1, j, heights[i][j], visited)
+            dfs(i - 1, j, heights[i][j], visited)
+            dfs(i, j + 1, heights[i][j], visited)
+            dfs(i, j - 1, heights[i][j], visited)
+            
+        for i in range(m):
+            dfs(i, 0, heights[i][0], pac)
+            dfs(i, n - 1, heights[i][n - 1], atl)
         
         for i in range(n):
-            dfs(0, i, pac, heights[0][i])
-            dfs(m - 1, i, atl, heights[m - 1][i])
-        
-        for i in range(m):
-            dfs(i, 0, pac, heights[i][0])
-            dfs(i, n - 1, atl, heights[i][n - 1])
+            dfs(0, i, heights[0][i], pac)
+            dfs(m - 1, i, heights[m - 1][i], atl)
         
         for i in range(m):
             for j in range(n):
                 if (i, j) in pac and (i, j) in atl:
-                    res.append((i, j))
+                    res.append([i, j])
         
         return res
