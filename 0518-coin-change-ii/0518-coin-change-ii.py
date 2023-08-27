d559fde2a7a6822bbc152011d8e79460aa90d7ba
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-27 13:05:03 +0900

    Time: 695 ms (18.80%), Space: 167.5 MB (8.69%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,18 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
+        # dfs solution
+        cache = {}
         
-        for coin in coins:
-            for j in range(coin, amount + 1):
-                dp[j] += dp[j - coin]
+        def dfs(i, a):
+            if i >= len(coins) or a > amount:
+                return 0
+            if a == amount:
+                return 1
+            if (i,a) in cache:
+                return cache[(i, a)]
+            
+            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+            
+            return cache[(i, a)]
         
-        return dp[amount]
+        return dfs(0, 0)
