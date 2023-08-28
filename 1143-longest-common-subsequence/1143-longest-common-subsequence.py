d2e33cb057338c842f01a3f25cc24fc690119984
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-28 15:28:55 +0900

    Time: 595 ms (82.04%), Space: 42.5 MB (35.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -4,7 +4,7 @@ class Solution:
         # Space: O(m * n)
         dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
         
-        for i in range(len(text1) -1, -1, -1):
+        for i in range(len(text1) - 1, -1, -1):
             for j in range(len(text2) -1, -1, -1):
                 if text1[i] == text2[j]:
                     dp[i][j] = 1 + dp[i + 1][j + 1]
