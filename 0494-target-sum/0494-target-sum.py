961be456ad3dc6ddfddd328cde2cf241d02a6929
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-28 17:31:53 +0900

    Time: 277 ms (52.08%), Space: 37.8 MB (38.65%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,33 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         # Time: O(-sum of num ~ sum of num)
-        dp = {}
+        dp = {} # (index, total)
         
-        def backtrack(idx, total):
-            if idx == len(nums):
+        def dfs(i, total):
+            if i == len(nums):
                 return 1 if total == target else 0
-            if (idx, total) in dp:
-                return dp[(idx, total)]
             
-            dp[(idx, total)] = (backtrack(idx+1, total+nums[idx]) + 
-                               backtrack(idx+1, total-nums[idx]))
+            if (i, total) in dp:
+                return dp[(i, total)]
             
-            return dp[(idx, total)]
-        
-        return backtrack(0,0)
-        # Time: O(2^n)
-        
-        res = [0]
-        
-        def backtrack(idx, total):
-            if idx == len(nums) and total == target:
-                res[0] += 1
-                return
-            if idx >= len(nums):
-                return
+            dp[(i, total)] = dfs(i + 1, total + nums[i]) + dfs(i + 1, total - nums[i])
             
-            backtrack(idx+1, total+nums[idx])
-            backtrack(idx+1, total-nums[idx])
-
-        backtrack(0,0)
-        return res[0]
+            return dp[(i, total)]
+        return dfs(0, 0)
+        
+        
+        # Time: O(2^n) -> Brute force
+        
