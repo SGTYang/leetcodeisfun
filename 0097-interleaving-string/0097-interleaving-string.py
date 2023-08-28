54a73beb33f8eae90c81afc544cc21fec9b1e1b0
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-08-28 17:56:40 +0900

    Time: 42 ms (83.64%), Space: 17.3 MB (37.82%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,17 +1,23 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        
+        # dfs, memoization
+        cache = {}
         if len(s1) + len(s2) != len(s3):
             return False
-        @cache
+        
         def dfs(i, j):
             if i == len(s1) and j == len(s2):
                 return True
-            choose_s1, choose_s2 = False, False
-            if i < len(s1) and s1[i] == s3[i + j]:
-                choose_s1 = dfs(i + 1, j)
-            if j < len(s2) and s2[j] == s3[i + j]:
-                choose_s2 = dfs(i, j + 1)
-
-            return choose_s1 or choose_s2
-
+            if (i, j) in cache:
+                return cache[(i, j)]
+            
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+            
+            cache[(i, j)] = False
+            return False
+        
         return dfs(0, 0)
