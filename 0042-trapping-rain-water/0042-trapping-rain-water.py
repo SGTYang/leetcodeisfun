b27d06016c3c8320491d9b6b6a60a5907ccb450a
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-09-19 17:37:37 +0900

    Time: 133 ms (22.75%), Space: 17.9 MB (99.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,44 +1,67 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-        l, r = 0, len(height) - 1
-        left_max, right_max = height[l], height[r]
-        res = 0
+        # Time complexity O(N)
+        # Space complexity O(n)
+        n = len(height)
+        max_left = [0 for i in range(n)]
+        max_right = [0 for i in range(n)]
         
-        while l < r:
-            if left_max < right_max:
-                l += 1
-                left_max = max(left_max, height[l])
-                res += left_max - height[l]
-            else:
-                r -= 1
-                right_max = max(right_max, height[r])
-                res += right_max - height[r]
+        curr = height[0]
+        for i in range(1, n):
+            curr = max(curr, height[i - 1])
+            max_left[i] = curr
+        
+        curr = height[-1]
+        for i in range(n - 2, -1, -1):
+            curr = max(curr, height[i + 1])
+            max_right[i] = curr
+        
+        res = 0
+        for i in range(n):
+            water = min(max_left[i], max_right[i]) - height[i]
+            res += water if water > 0 else 0
         
         return res
-        
-        # Time complexity: O(n)
-        # Space complexity: O(n)
-        n = len(height)
-        max_r = [0 for i in range(n)]
-        max_l = [0 for i in range(n)]
-        res = [0 for i in range(n)]
-        
-        curr_max = height[0]
-        for i in range(1, n):
-            curr_max = max(curr_max, height[i-1])
-            max_l[i] = curr_max
             
-        curr_max = height[n-1]
-        for i in range(n-2,-1,-1):
-            curr_max = max(curr_max, height[i+1])
-            max_r[i] = curr_max
+#         if not height:
+#             return 0
+#         l, r = 0, len(height) - 1
+#         left_max, right_max = height[l], height[r]
+#         res = 0
         
-        for i in range(n):
-            water = min(max_l[i], max_r[i]) - height[i]
-            if water < 0:
-                water = 0
-            res[i] = water
+#         while l < r:
+#             if left_max < right_max:
+#                 l += 1
+#                 left_max = max(left_max, height[l])
+#                 res += left_max - height[l]
+#             else:
+#                 r -= 1
+#                 right_max = max(right_max, height[r])
+#                 res += right_max - height[r]
         
-        return sum(res)
+#         return res
+        
+#         # Time complexity: O(n)
+#         # Space complexity: O(n)
+#         n = len(height)
+#         max_r = [0 for i in range(n)]
+#         max_l = [0 for i in range(n)]
+#         res = [0 for i in range(n)]
+        
+#         curr_max = height[0]
+#         for i in range(1, n):
+#             curr_max = max(curr_max, height[i-1])
+#             max_l[i] = curr_max
+            
+#         curr_max = height[n-1]
+#         for i in range(n-2,-1,-1):
+#             curr_max = max(curr_max, height[i+1])
+#             max_r[i] = curr_max
+        
+#         for i in range(n):
+#             water = min(max_l[i], max_r[i]) - height[i]
+#             if water < 0:
+#                 water = 0
+#             res[i] = water
+        
+#         return sum(res)
