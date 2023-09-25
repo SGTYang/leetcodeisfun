03fe23709cb94e125beb20a08472a7760d9e15ac
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-09-25 16:44:49 +0900

    Time: 97 ms (23.84%), Space: 16.6 MB (27.24%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -2,26 +2,26 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
+        
         l1, l2 = len(nums1), len(nums2)
         total = l1 + l2
         half = total // 2
         
-        l, r = 0, l1 - 1
+        left, right = 0, l1 - 1
         while True:
-            mid = (l + r) // 2
-            rest = half - mid - 2
+            mid = (left + right) // 2
+            idx = half - mid - 2
             
-            nums1_left = nums1[mid] if mid >= 0 else float('-inf')
+            nums1_left = nums1[mid] if mid >=0 else float('-inf')
             nums1_right = nums1[mid + 1] if mid + 1 < len(nums1) else float('inf')
-            nums2_left = nums2[rest] if rest >= 0 else float('-inf')
-            nums2_right = nums2[rest + 1] if rest + 1 < len(nums2) else float('inf')
+            nums2_left = nums2[idx] if idx >= 0 else float('-inf')
+            nums2_right = nums2[idx + 1] if idx + 1 < len(nums2) else float('inf')
             
-            if nums1_left <= nums2_right and nums2_left <= nums1_right:
+            if nums1_left <= nums2_right and nums1_right >= nums2_left:
                 if total % 2:
                     return min(nums1_right, nums2_right)
-                return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
+                return (max(nums1_left, nums2_left) + min(nums2_right, nums1_right)) / 2
             elif nums1_left > nums2_right:
-                r = mid - 1
+                right = mid - 1
             else:
-                l = mid + 1
-        
+                left = mid + 1
