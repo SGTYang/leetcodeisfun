7110c0466fb46ab1cf7556b2950619cacad85c2f
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-10-04 17:53:59 +0900

    Time: 102 ms (79.01%), Space: 17.1 MB (82.65%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "": 
-            return ""
-        s_count = defaultdict(int)
-        t_count = defaultdict(int)
+        t_dict = defaultdict(int)
+        window_dict = defaultdict(int)
         
-        for i in range(len(t)):
-            t_count[t[i]] += 1
+        for c in t:
+            t_dict[c] += 1
             
-        have, need = 0, len(t_count)
-        l = 0
-        min_len = float('inf')
-        res = [0, 0]
+        have, need = 0, len(t_dict)
         
+        res_len = float('inf')
+        res = [-1, -1]
+        
+        l = 0
         for r in range(len(s)):
-            s_count[s[r]] += 1
-            if s[r] in t_count and s_count[s[r]] == t_count[s[r]]:
+            window_dict[s[r]] += 1
+            if s[r] in t_dict and window_dict[s[r]] == t_dict[s[r]]:
                 have += 1
             
             while have == need:
-                if r - l + 1 < min_len:
-                    min_len = r - l + 1
+                if res_len > r - l + 1:
+                    res_len = r - l + 1
                     res = [l, r]
-                s_count[s[l]] -= 1
-                if s[l] in t_count and s_count[s[l]] < t_count[s[l]]:
+                    
+                window_dict[s[l]] -= 1
+                
+                if s[l] in t_dict and window_dict[s[l]] < t_dict[s[l]]:
                     have -= 1
+                    
                 l += 1
         
-        return s[res[0]:res[1] + 1] if min_len != float('inf') else ""
+        return s[res[0]:res[1]+1] if res_len != float('inf') else ""
