c507ed106612ef8ec388a34ad54bbc864419e9d9
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-10-06 18:49:46 +0900

    Time: 100 ms (82.62%), Space: 17.1 MB (47.81%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,17 +1,17 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_dict = defaultdict(int)
         window_dict = defaultdict(int)
+        t_dict = defaultdict(int)
         
         for c in t:
             t_dict[c] += 1
             
-        have, need = 0, len(t_dict)
-        
-        res_len = float('inf')
-        res = [-1, -1]
-        
+        need = len(t_dict)
+        have = 0
         l = 0
+        res = [-1, -1]
+        res_len = float('inf')
+        
         for r in range(len(s)):
             window_dict[s[r]] += 1
             if s[r] in t_dict and window_dict[s[r]] == t_dict[s[r]]:
@@ -21,7 +21,7 @@ class Solution:
                 if res_len > r - l + 1:
                     res_len = r - l + 1
                     res = [l, r]
-                    
+                
                 window_dict[s[l]] -= 1
                 
                 if s[l] in t_dict and window_dict[s[l]] < t_dict[s[l]]:
@@ -29,4 +29,4 @@ class Solution:
                     
                 l += 1
         
-        return s[res[0]:res[1]+1] if res_len != float('inf') else ""
+        return s[res[0]:res[1] + 1] if res_len != float('inf') else ""
