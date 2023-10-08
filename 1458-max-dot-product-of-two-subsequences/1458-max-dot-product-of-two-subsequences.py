d85b3b6b6a1c6a86c8533126417db221124d5ba6
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-10-08 10:55:50 +0900

    Time: 266 ms (66.20%), Space: 23.7 MB (50.00%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
         m, n = len(nums1), len(nums2)
-        dp = [[float('-inf') for j in range(n + 1)] for i in range(m + 1)]
+        dp = [[nums1[i] * nums2[j] for j in range(n)] for i in range(m)]
         
-        for i in range(m + 1):
-            for j in range(n + 1):
-                if i == 0 or j == 0:
-                    dp[i][j] = float('-inf')
-                else:
-                    curr = nums1[i - 1] * nums2[j - 1] + max(dp[i - 1][j - 1], 0)
-                    dp[i][j] = max(curr, dp[i - 1][j], dp[i][j - 1])
+        for i in range(1, m):
+            dp[i][0] = max(dp[i][0], dp[i - 1][0])
         
-        return dp[m][n]
+        for i in range(1, n):
+            dp[0][i] = max(dp[0][i], dp[0][i - 1])
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                curr = dp[i][j] + max(dp[i - 1][j - 1], 0)
+                dp[i][j] = max(curr, dp[i - 1][j], dp[i][j - 1])
+
+        return dp[-1][-1]
