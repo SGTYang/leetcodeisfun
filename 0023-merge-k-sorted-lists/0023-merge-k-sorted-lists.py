340ef218b534d278612aafd75a34b957500ebcf3
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-10-17 08:22:36 +0900

    Time: 112 ms (27.53%), Space: 19.9 MB (79.52%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -11,28 +11,27 @@ class Solution:
         def merge(l1, l2):
             dummy = ListNode(0)
             curr = dummy
-
+            
             while l1 or l2:
                 l1_val = l1.val if l1 else float('inf')
                 l2_val = l2.val if l2 else float('inf')
-                if l1_val > l2_val:
-                    curr.next = l2
-                    l2 = l2.next if l2 else None
-                else:
+                if l1_val < l2_val:
                     curr.next = l1
-                    l1 = l1.next if l1 else None
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
                 curr = curr.next
             
             return dummy.next
         
         while len(lists) > 1:
-            merged_list = []
+            dummy_list = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if i + 1 < len(lists) else None
-                merged_list.append(merge(l1, l2))
+                dummy_list.append(merge(l1, l2))
             
-            lists = merged_list
+            lists = dummy_list
         
         return lists[0]
-            
