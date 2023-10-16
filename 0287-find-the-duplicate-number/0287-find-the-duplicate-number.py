5495b1543c99e090dacb9242791a738c56a4acc6
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-10-16 16:20:53 +0900

    Time: 559 ms (27.21%), Space: 31 MB (44.21%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # 2(p+c-x) = p+nc-x
-        
         slow, fast = 0, 0
         
         while True:
@@ -9,12 +8,10 @@ class Solution:
             fast = nums[nums[fast]]
             if slow == fast:
                 break
-                
+
         curr = 0
-        while True:
+        while curr != slow:
             curr = nums[curr]
             slow = nums[slow]
-            if curr == slow:
-                break
-            
+        
         return slow
