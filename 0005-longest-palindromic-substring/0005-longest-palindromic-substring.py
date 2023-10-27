e913e6811f30c2d1e7adc149bc074f949c4f5c9a
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-10-27 17:56:03 +0900

    Time: 477 ms (65.09%), Space: 16.2 MB (95.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,21 +2,22 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         # Time: O(n ^ 2)
         # Space: O(1)
-        max_len = 0
         res = ""
+        max_len = 0
         for i in range(len(s)):
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > max_len:
-                    max_len = r - l + 1
-                    res = s[l : r + 1]
-                l -= 1
-                r += 1
-            l, r = i, i+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > max_len:
-                    max_len = r - l + 1
-                    res = s[l : r + 1]
-                l -= 1
-                r += 1
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max_len:
+                    res = s[left:right + 1]
+                    max_len = right - left + 1
+                left -= 1
+                right += 1
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max_len:
+                    res = s[left:right + 1]
+                    max_len = right - left + 1
+                left -= 1
+                right += 1
+        
         return res
