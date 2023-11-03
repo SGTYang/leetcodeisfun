beef295148af78608b7b7829a81517b426efb9ec
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-03 17:48:01 +0900

    Time: 6376 ms (5.01%), Space: 19.8 MB (92.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,33 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
-            return None
         
         def merge(l1, l2):
-            dummy = ListNode(0)
+            dummy = ListNode()
             curr = dummy
             
             while l1 or l2:
                 l1_val = l1.val if l1 else float('inf')
                 l2_val = l2.val if l2 else float('inf')
-                if l1_val < l2_val:
-                    curr.next = l1
-                    l1 = l1.next
-                else:
+                
+                if l1_val > l2_val:
                     curr.next = l2
                     l2 = l2.next
+                else:
+                    curr.next = l1
+                    l1 = l1.next
+                
                 curr = curr.next
+            
+            if l1:
+                curr.next = l1
+            if l2:
+                curr.next = l2
             
             return dummy.next
         
         while len(lists) > 1:
-            dummy_list = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                dummy_list.append(merge(l1, l2))
-            
-            lists = dummy_list
+            l1 = lists.pop()
+            l2 = lists.pop() if lists else None
+            lists.append(merge(l1, l2))
         
-        return lists[0]
+        return lists[0] if lists else None
+            
+            
