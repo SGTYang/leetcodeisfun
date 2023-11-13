14b595278fa0ef46cb7505a6341f6f65dbd28733
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-13 10:28:16 +0900

    Time: 163 ms (47.45%), Space: 18 MB (98.63%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,5 +1,30 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
+        # Time O(n)
+        # Space O(1)
+        vowel = "AEIOUaeiou"
+        vowel_cnt = [0]*len(vowel)
+        vowels = {"A":0, "E":1, "I":2, "O":3, "U":4, "a":5, "e":6, "i":7, "o":8, "u":9}
+        
+        for c in s:
+            if c in vowels:
+                vowel_cnt[vowels[c]] += 1
+        
+        res = ""
+        idx = 0
+        for c in s:
+            if c in vowels:
+                while vowel_cnt[idx] == 0:
+                    idx += 1
+                vowel_cnt[idx] -= 1
+                res += vowel[idx]
+            else:
+                res += c
+        
+        return res
+        
+        # Time O(nlogn)
+        # Space O(1)
         vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
         vow_list = [c for c in s if c in vowels]
         vow_list.sort(reverse=True)
