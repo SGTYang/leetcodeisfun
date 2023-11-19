e766acdcc4d74583ead033ab83da0a75eafac5ae
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-19 15:34:59 +0900

    Time: 44 ms (79.19%), Space: 18.6 MB (62.22%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -10,9 +10,11 @@ class Solution:
         def dfs(node):
             if not node:
                 return 0
+            
             left = dfs(node.left)
             right = dfs(node.right)
-            res[0] = max(res[0], left + right)
+            
+            res[0] = max(left + right, res[0])
             
             return max(left, right) + 1
         dfs(root)
