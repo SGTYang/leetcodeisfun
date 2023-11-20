5010642ea4d5f77ff63c3a71e400d1c59aff47b0
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-20 15:43:27 +0900

    Time: 56 ms (39.75%), Space: 20.5 MB (22.84%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,6 +6,19 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            if len(res) == k:
+                return
+            res.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        return res[-1]
+
         # Iterative
         n = 0
         stack = []
