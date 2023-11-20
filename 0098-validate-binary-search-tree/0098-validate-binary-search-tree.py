0035baca16387ff8067c3e12a6f212a2314a7002
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-20 12:36:27 +0900

    Time: 52 ms (30.80%), Space: 18.5 MB (98.83%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -9,8 +9,10 @@ class Solution:
         def dfs(node, left, right):
             if not node:
                 return True
+            
             if not (left < node.val < right):
                 return False
+            
             return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
         
         return dfs(root, float('-inf'), float('inf'))
