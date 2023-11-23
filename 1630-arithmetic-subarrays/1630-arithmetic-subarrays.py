b72fa9f8f5089ddd652ec3ec2a01ee7cb844474f
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-23 11:58:31 +0900

    Time: 157 ms (90.20%), Space: 16.5 MB (69.71%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -5,6 +5,33 @@ class Solution:
         for i in range(len(l)):
             start = l[i]
             end = r[i]
+            q_range = nums[start : end+1]
+            min_num = min(q_range)
+            max_num = max(q_range)
+            if (max_num - min_num) % (len(q_range) - 1) != 0:
+                res.append(False)
+                continue
+            diff = (max_num - min_num) // (len(q_range) - 1)
+            
+            q_range_set = set(q_range)
+            curr = min_num + diff
+            is_arith = True
+            while curr < max_num:
+                if curr not in q_range_set:
+                    is_arith = False
+                    break
+                curr += diff
+            
+            res.append(is_arith)
+             
+        return res
+    
+#############
+        res = []
+        
+        for i in range(len(l)):
+            start = l[i]
+            end = r[i]
             elem = sorted(nums[start : end+1])
             is_arith = True
             if len(elem) >= 2:
