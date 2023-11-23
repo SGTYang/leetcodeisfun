4db7443646e20a3fbf60b2a5e0aab5757e9a9f31
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-23 13:55:38 +0900

    Time: 46 ms (38.14%), Space: 16.5 MB (28.63%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -3,20 +3,19 @@ class Solution:
         # Time: O(n*2^n) -> n for subset.copy()
         # Space: O(n)
         nums.sort()
+        
         res = []
-        curr = []
-        def dfs(idx):
+        def dfs(idx, curr):
             if idx == len(nums):
                 res.append(curr.copy())
                 return
             
             curr.append(nums[idx])
-            dfs(idx + 1)
+            dfs(idx + 1, curr)
             curr.pop()
-            
             while idx + 1 < len(nums) and nums[idx] == nums[idx + 1]:
                 idx += 1
-            dfs(idx + 1)
-        
-        dfs(0)
-        return res 
+            dfs(idx + 1, curr)
+            
+        dfs(0, [])
+        return res
