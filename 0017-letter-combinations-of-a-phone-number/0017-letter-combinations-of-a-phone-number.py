9f80c5294f908f9251ccce6338715d7b0b92ca56
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-11-26 12:30:33 +0900

    Time: 45 ms (13.89%), Space: 16.3 MB (62.25%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,32 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        num_dict = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+        dig = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz",
         }
+        
         res = []
-        curr = []
-        def dfs(idx):
-            if len(curr) == len(digits):
+        
+        def dfs(idx, curr):
+            if idx == len(digits):
                 res.append("".join(curr))
                 return
-            if idx >= len(digits):
-                return
             
-            for c in num_dict[digits[idx]]:
+            for c in dig[digits[idx]]:
                 curr.append(c)
-                dfs(idx+1)
+                dfs(idx + 1, curr)
                 curr.pop()
         
-        dfs(0)
-        if digits == "":
-            return []
+        dfs(0, [])
         
-        return res
-                    
+        return res if digits else []
