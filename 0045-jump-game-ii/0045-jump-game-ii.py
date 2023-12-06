187e806a6e8d04fccc75dda76f9684ef6890ef1c
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-06 17:50:06 +0900

    Time: 12313 ms (5.01%), Space: 17.5 MB (28.50%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,16 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         # Greedy
-        l, r = 0, 0
-        res = 0
+        jumps = [float('inf')] * len(nums)
+        jumps[0] = 0
         
-        while r < len(nums) - 1:
-            farthest = 0
-            for i in range(l, r + 1):
-                farthest = max(farthest, i + nums[i])
-            
-            l = r + 1
-            r = farthest
-            res += 1
-        return res
-    
+        for i in range(len(nums)):
+            for j in range(1, nums[i] + 1):
+                jumps[min(i + j, len(nums) - 1)] = min(jumps[min(i + j, len(nums) - 1)], jumps[i] + 1)
+        
+        return jumps[-1]
