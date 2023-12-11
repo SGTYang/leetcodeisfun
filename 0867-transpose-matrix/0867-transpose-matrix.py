54b5ab1ae9adc41cbe1012be1236551bae4893a2
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-11 10:34:31 +0900

    Time: 70 ms (78.34%), Space: 17.1 MB (53.63%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        if not matrix:
+            return []
+        
+        res = []
+        
+        for i in range(len(matrix[0])):
+            row = []
+            for j in range(len(matrix)):
+                row.append(matrix[j][i])
+            res.append(row)
+        
+        return res
