ccf1125550d615d4cda6145cc420e39e8cb6ec74
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-13 16:46:48 +0900

    Time: 34 ms (85.06%), Space: 16.3 MB (33.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -3,12 +3,16 @@ class Solution:
         # Time: O(m * n)
         # Space: O(m * n)
         
-        row = [1] * (n + 1)
+        dp = [[0] * n for i in range(m)]
         
-        for i in range(m - 1):
-            tmp_row = [0] * (n + 1)
-            for j in range(n - 1, -1, -1):
-                tmp_row[j] = tmp_row[j + 1] + row[j]
-            row = tmp_row
+        for i in range(n):
+            dp[0][i] = 1
         
-        return row[0]
+        for i in range(m):
+            dp[i][0] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[-1][-1]
