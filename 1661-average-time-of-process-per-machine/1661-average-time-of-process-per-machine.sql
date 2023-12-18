8b040c630e69dd6b88ed044d2995922eb0ba16d1
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-18 17:58:36 +0900

    Time: 388 ms (67.93%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -7,10 +7,10 @@
 # AND a2.activity_type = "end"
 # GROUP BY 1;
 
-SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) as processing_time
-FROM Activity a1 JOIN Activity a2 
-ON a1.machine_id = a2.machine_id 
-AND a1.process_id = a2.process_id 
-AND a1.activity_type = "start" 
-AND a2.activity_type = "end"
-GROUP BY 1;
+select a2.machine_id, round(avg(a2.timestamp - a1.timestamp), 3) as processing_time
+from Activity as a1, Activity as a2
+where a1.process_id = a2.process_id
+and a1.machine_id = a2.machine_id
+and a1.activity_type = "start"
+and a2.activity_type = 'end'
+group by a2.machine_id;
