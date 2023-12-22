df3c4c0dbd06c0fd1cd6c09a7e49108fdd8d041e
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-22 14:53:12 +0900

    Time: 37 ms (99.89%), Space: 17.5 MB (15.98%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,28 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        visited = set()
         que = deque([])
-        directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
-        rows, cols = len(grid), len(grid[0])
+        time = 0
+        fresh = 0
+        ROW, COL = len(grid), len(grid[0])
         
-        fresh = time = 0
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(ROW):
+            for j in range(COL):
                 if grid[i][j] == 1:
                     fresh += 1
                 elif grid[i][j] == 2:
-                    que.append((i, j))
-                
+                    que.append([i, j])
+                    visited.add((i, j))
+        
         while que and fresh:
             n = len(que)
             for _ in range(n):
                 r, c = que.popleft()
+            
                 for y, x in directions:
-                    n_y = y + r
-                    n_x = x + c
-                    if 0 <= n_y < rows and 0 <= n_x < cols and grid[n_y][n_x] == 1:
-                        fresh -= 1
-                        grid[n_y][n_x] = 2
-                        que.append((n_y, n_x))
+                    n_r, n_c = y + r, x + c
+                    
+                    if n_r < 0 or n_c < 0 or n_r >= ROW or n_c >= COL or (n_r, n_c) in visited or grid[n_r][n_c] == 0:
+                        continue
+                    visited.add((n_r, n_c))
+                    que.append((n_r, n_c))
+                    grid[n_r][n_c] = 2
+                    fresh -= 1
+                    
             time += 1
         
         return time if not fresh else -1
