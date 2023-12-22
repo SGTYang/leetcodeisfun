64ee7210323b232b46c226482fa36e6c441aede2
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-22 10:15:02 +0900

    Time: 933 ms (97.82%), Space: 52 MB (63.36%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,26 +1,25 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        city_dict = {city:[] for city in range(n)}
+        res = [0]
+        roads = defaultdict(list)
+        ways = set()
         visited = set()
-        ans = 0
-        city_set = {(a,b) for a,b in connections}
-
-        for a,b in connections:
-            city_dict[a].append(b)
-            city_dict[b].append(a)
-
         
-        def dfs(city):
-            nonlocal visited, city_dict, ans, city_set
+        
+        for start, end in connections:
+            roads[start].append(end)
+            roads[end].append(start)
+            ways.add((start, end))
+        
+        def dfs(node):
+            visited.add(node)
             
-            for a in city_dict[city]:
-                if a in visited:
-                    continue
-                if (a, city) not in city_set:
-                    ans += 1
-                visited.add(a)
-                dfs(a)
-            
-        visited.add(0)
+            for n in roads[node]:
+                if n not in visited:
+                    if (n, node) not in ways:
+                        res[0] += 1
+                    dfs(n)
+
         dfs(0)
-        return ans
+        
+        return res[0]
