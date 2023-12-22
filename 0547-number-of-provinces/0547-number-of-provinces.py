af57032360f8bbb28b13e608e7fbb4134764c71c
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-22 09:55:09 +0900

    Time: 191 ms (99.17%), Space: 18.4 MB (8.29%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,35 +1,27 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        par = [i for i in range(len(isConnected))]
-        rank = [1]*len(isConnected)
-        res = len(isConnected)
         
-        def find(node):
-            res = node
-            while res != par[res]:
-                par[res] = par[par[res]]
-                res = par[res]
-            
-            return res
-        
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-            
-            if p1 == p2:
-                return 0
-            
-            if rank[p1] > rank[p2]:
-                par[p2] = p1
-                rank[p1] += rank[p2]
-            else:
-                par[p1] = p2
-                rank[p2] += rank[p1]
-            
-            return 1
+        province = defaultdict(list)
         
         for i in range(len(isConnected)):
             for j in range(len(isConnected[0])):
                 if isConnected[i][j] == 1:
-                    res -= union(i, j)
+                    province[i].append(j)
+                    province[j].append(i)
+        
+        res = 0
+        visited = set()
+        
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for n in province[node]:
+                dfs(n)
+        
+        for n in province.keys():
+            if n not in visited:
+                res += 1
+                dfs(n)
         
         return res
