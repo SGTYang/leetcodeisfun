e6cb101c904a94db330b4946039a14a750cd9f2b
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2023-12-27 08:51:16 +0900

    Time: 1666 ms (61.99%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select r.contest_id, round(count(u.user_id)/(select count(distinct(user_id)) from Users)*100, 2) as percentage
+select r.contest_id, round(count(u.user_id) / (select count(user_id) from Users) * 100, 2) as percentage
 from Register as r
     left join Users as u
     on r.user_id = u.user_id
