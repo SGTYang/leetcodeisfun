0086b9b537cad6fb9a0b4f7be30132b85a373edf
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2024-01-08 15:08:34 +0900

    Time: 578 ms (64.65%), Space: 72.1 MB (26.43%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -1,21 +1,12 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        nums2mp = collections.defaultdict(list)
-        n = len(nums)
-        dp = [[0] * n for _ in range(n)]
         res = 0
-        for i, num in enumerate(nums):
-            nums2mp[num].append(i)
-
-        for i in range(n):
+        dp = [defaultdict(int) for _ in range(len(nums))]
+        
+        for i in range(len(nums)):
             for j in range(i):
-                prev = 2 * nums[j] - nums[i]
-                if prev > sys.maxsize or prev < -sys.maxsize - 1:
-                    continue
-                prev_indices = nums2mp.get(prev, [])
-                for k in prev_indices:
-                    if k >= j:
-                        break
-                    dp[i][j] += dp[j][k] + 1
-                res += dp[i][j]
+                diff = nums[i] - nums[j]
+                dp[i][diff] += dp[j][diff] + 1
+                res += dp[j][diff]
+        
         return res
