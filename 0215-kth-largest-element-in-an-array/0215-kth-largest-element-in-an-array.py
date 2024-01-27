e1eefbd6ac90a86a9c5add677bfda9b0d31d2f82
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2024-01-28 04:03:41 +0900

    Time: 514 ms (45.89%), Space: 29.6 MB (76.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,14 +1,20 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # Time: O(n)
-        # Quick select
+        # Does this input array contains negative numbers?
+        # Does this input array contains duplicate numbers?
+        # [-1, 2, 3, 4, 5, 0, 2, -8]
+        # Can I use built in python function
         
+        # Heap
+        # [-5, -4, -3, -2, -2, 0, 1, 8]
+        # k = 3 -> res = 5 k = 2, res = 4 k = 1, res = 3 k =0
+        # O(N + Klogn)
+        heap = []
+        for n in nums:
+            heapq.heappush(heap, -n)
         
-        # Time O(klogn)
-        # Max heap
-        heapq.heapify(nums)
-        
-        while len(nums) > k:
-            heapq.heappop(nums)
-        
-        return nums[0]
+        while k:
+            res = -heapq.heappop(heap)
+            k -= 1
+            
+        return res
