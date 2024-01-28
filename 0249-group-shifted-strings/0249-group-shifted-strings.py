096e38dbb7f545381902ef7affeb1b0bf23bc8ff
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2024-01-28 23:39:01 +0900

    Time: 35 ms (90.68%), Space: 16.6 MB (68.51%) - LeetHub

---
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        # "abc", "ace" - > diff group
+        # create dict, k: (diff) v: list
+        diff = defaultdict(list)
+        
+        for string in strings:
+            if len(string) == 1:
+                diff[(-1)].append(string)
+            else:
+                tmp = []
+                prev = string[0]
+                for i in range(1, len(string)):
+                    res = ord(prev) - ord(string[i])
+                    if res < 0:
+                        res += 26
+                    tmp.append(res)
+                    prev = string[i]
+                diff[tuple(tmp)].append(string)
+        
+        return list(diff.values())
