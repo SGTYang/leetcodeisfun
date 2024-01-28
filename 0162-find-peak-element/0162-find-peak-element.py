8ffd379b836ce9222cf9992487e6c0e3fd6044ce
Jaeho Yang <42399580+SGTYang@users.noreply.github.com>
2024-01-28 23:13:40 +0900

    Time: 48 ms (63.54%), Space: 16.8 MB (56.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,23 +1,20 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         # Adjacent neighbors are not same
-        if len(nums) < 2 or nums[0] > nums[1]:
-            return 0
-        
-        elif nums[-1] > nums[-2]:
-            return len(nums) - 1
+        # go thorugh all the elements and check their neighbors
+        # [1, 9, 10, 2, 4, -1, 2, -4]
         
         left, right = 0, len(nums) - 1
         
-        while left < right:
+        while left <= right:
             mid = (left + right) // 2
             
-            if nums[mid-1] < nums[mid] > nums[mid + 1]:
-                return mid
-            
-            elif nums[mid - 1] > nums[mid]:
+            # left nei is greater
+            if mid > 0 and nums[mid] < nums[mid - 1]:
                 right = mid - 1
-            else:
+            # right nei is greater
+            elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
                 left = mid + 1
+            else:
+                return mid
         
-        return left
